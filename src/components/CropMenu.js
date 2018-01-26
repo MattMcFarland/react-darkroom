aeb2186c1aefc5d351947f1029baf56e46d08540
Alex Autem <autem.alex@gmail.com>
2018-01-25 19:17:07 -0600

    Update unrecognized prop on CropMenu component

--- src/components/CropMenu.js
+++ src/components/CropMenu.js
@@ -4,7 +4,7 @@ export default ({isCropping, children}) =>
   <span>
     {
       React.Children.map(children, child => {
-        const showOnlyWhen = child.props['data-showOnlyWhen'];
+        const showOnlyWhen = child.props['data-showonlywhen'];
 
         if (isCropping && showOnlyWhen === "croppingIsOn") {
           return child;
